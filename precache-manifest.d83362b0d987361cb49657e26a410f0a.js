93763bdedff57baef2ca92e69e8b09ab17d5ab09
nhaouari <nhaouari@users.noreply.github.com>
2018-11-15 16:08:18 +0100

    update links

---
+++ precache-manifest.d83362b0d987361cb49657e26a410f0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96e279704cb15ccd4039",
+    "url": "/static/js/main.96e27970.chunk.js"
+  },
+  {
+    "revision": "e9fc1a442c7b2c82f325",
+    "url": "/static/js/1.e9fc1a44.chunk.js"
+  },
+  {
+    "revision": "96e279704cb15ccd4039",
+    "url": "/static/css/main.3c9f3ec7.chunk.css"
+  },
+  {
+    "revision": "ca1ef271c421f2bf46095b10bd92c425",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CRATE/precache-manifest.9fa178f62ef39173ac8037cdd70f4390.js"
+  "/precache-manifest.d83362b0d987361cb49657e26a410f0a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/CRATE/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
